44b6061e4f34885107cc74249e322ca945403dae
Reuben <reubenvandorp@gmail.com>
2022-01-26 13:02:10 +1300

    Added more to lagrangeInterpolation.cpp

--- include/LagrangeInterpolation.h
+++ include/LagrangeInterpolation.h
@@ -3,19 +3,33 @@
 
 #define NUM_POINTS 5 // Number of points to interpolate through
 
+/**
+ * 
+ * Use the linked graphical demonstration to choose ALPHA and BETA values, there 
+ * are sliders to change these values.
+ * 
+ * Graphical demonstration ==> https://www.desmos.com/calculator/t7gsylwjal
+ * - A larger ALPHA value increases the initial steepness of the z-curve
+ * - A larger BETA increases the height of intermediate control point 
+ */
+#define FUNCTION_SHAPE_ALPHA 0.6; // MUST BE IN RANGE!! [0.5, 0.77]
+#define FUNCTION_SHAPE_BETA  0.4; // MUST BE IN RANGE!! [0.25, 0.5]
+
+
 class Lagrange_5pnt {
     public:
         Lagrange_5pnt();
         double get_z_value(double t);
 
     private:
-        double t[5]; // Parameter L(t) where t is the distance between position and setpoint sqrt(x^2 + y^2)
-        double z[5]; // z (vertical) leg position
+        double t_pnt[5]; // Parameter L(t) where t is the distance between position and setpoint sqrt(x^2 + y^2)
+        double z_pnt[5]; // z (vertical) leg position
+        double param[5];
 
         double x_intermediate_offset;
         double y_intermediate_offset;
         
-        void set_lagrange_parameters();
+        void set_lagrange_parameters(double max_z);
 
 };
 
--- src/LagrangeInterpolation.cpp
+++ src/LagrangeInterpolation.cpp
@@ -19,13 +19,43 @@ Lagrange_5pnt::Lagrange_5pnt()
     
 }
 
-void Lagrange_5pnt::set_lagrange_parameters()
+void Lagrange_5pnt::set_lagrange_parameters(double max_z)
 {
+    double alpha = FUNCTION_SHAPE_ALPHA;
+    double beta =  FUNCTION_SHAPE_BETA;
 
+    z_pnt[0] = 0;
+    z_pnt[2] = max_z; 
+    double z_offset = beta * (z_pnt[2] - z_pnt[0]);
+    
+    t_pnt[4] = 0; // *t_max?*
+
+    t_pnt[0] = 0;
+    t_pnt[2] = (t_pnt[0]+t_pnt[4]) / 2;
+    double t_offset = alpha * (t_pnt[2] - t_pnt[0]);
+    
+    t_pnt[1] = t_pnt[2] - t_offset;
+    t_pnt[3] = t_pnt[2] + t_offset;
+
+    z_pnt[1] = z_pnt[2] - z_offset;
+    z_pnt[3] = z_pnt[1];
+    z_pnt[4] = z_pnt[0];
+
+    param[0] = z_pnt[0] / (t_pnt[0]-t_pnt[1])*(t_pnt[0]-t_pnt[2])*(t_pnt[0]-t_pnt[3])*(t_pnt[0]-t_pnt[4]);
+    param[1] = z_pnt[1] / (t_pnt[1]-t_pnt[0])*(t_pnt[1]-t_pnt[2])*(t_pnt[1]-t_pnt[3])*(t_pnt[1]-t_pnt[4]);    
+    param[2] = z_pnt[2] / (t_pnt[2]-t_pnt[0])*(t_pnt[2]-t_pnt[1])*(t_pnt[2]-t_pnt[3])*(t_pnt[2]-t_pnt[4]);    
+    param[3] = z_pnt[3] / (t_pnt[3]-t_pnt[0])*(t_pnt[3]-t_pnt[1])*(t_pnt[3]-t_pnt[2])*(t_pnt[3]-t_pnt[4]);
+    param[4] = z_pnt[4] / (t_pnt[4]-t_pnt[0])*(t_pnt[4]-t_pnt[1])*(t_pnt[4]-t_pnt[2])*(t_pnt[4]-t_pnt[3]);
 }
 
 double Lagrange_5pnt::get_z_value(double t)
 {
-    t = {0,0,0,0,0};
-    return (t-t[1])*(t-t[2])*(t-t[3])*(t-t[4]);
+    double return_value =
+    (t-t_pnt[1])*(t-t_pnt[2])*(t-t_pnt[3])*(t-t_pnt[4]) * param[0] +
+    (t-t_pnt[0])*(t-t_pnt[2])*(t-t_pnt[3])*(t-t_pnt[4]) * param[0] +
+    (t-t_pnt[0])*(t-t_pnt[1])*(t-t_pnt[3])*(t-t_pnt[4]) * param[0] +
+    (t-t_pnt[0])*(t-t_pnt[1])*(t-t_pnt[2])*(t-t_pnt[4]) * param[0] +
+    (t-t_pnt[0])*(t-t_pnt[1])*(t-t_pnt[2])*(t-t_pnt[3]) * param[0];
+
+    return return_value;
 }
