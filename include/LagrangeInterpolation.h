c8c7ef7e129f56d0a686dcfc4bc7dc63fc066fe9
Reuben <reubenvandorp@gmail.com>
2022-01-27 08:24:15 +1300

    ready for testing

--- include/LagrangeInterpolation.h
+++ include/LagrangeInterpolation.h
@@ -4,7 +4,6 @@
 #define NUM_POINTS 5 // Number of points to interpolate through
 
 /**
- * 
  * Use the linked graphical demonstration to choose ALPHA and BETA values, there 
  * are sliders to change these values.
  * 
@@ -12,25 +11,25 @@
  * - A larger ALPHA value increases the initial steepness of the z-curve
  * - A larger BETA increases the height of intermediate control point 
  */
-#define FUNCTION_SHAPE_ALPHA 0.6; // MUST BE IN RANGE!! [0.5, 0.77]
+#define FUNCTION_SHAPE_ALPHA 0.7; // MUST BE IN RANGE!! [0.5, 0.77]
 #define FUNCTION_SHAPE_BETA  0.4; // MUST BE IN RANGE!! [0.25, 0.5]
 
-
+/**
+ * @brief Usage:
+ * - Create new Lagrange_5pnt oject using "Lagrange_5pnt l;"
+ * - Call the set parameters function using "l.set_parameters(double max_z, double setpoint_distance)"
+ * - When you want an updated Z-coordinate, call "get_z_value(double distance_to_setpoint)"
+ */
 class Lagrange_5pnt {
     public:
         Lagrange_5pnt();
-        double get_z_value(double t);
+        void set_lagrange_parameters(double max_z, double setpoint_distance);
+        double get_z_value(double setpoint_distance);
 
     private:
         double t_pnt[5]; // Parameter L(t) where t is the distance between position and setpoint sqrt(x^2 + y^2)
         double z_pnt[5]; // z (vertical) leg position
         double param[5];
-
-        double x_intermediate_offset;
-        double y_intermediate_offset;
-        
-        void set_lagrange_parameters(double max_z);
-
 };
 
 #endif
--- src/LagrangeInterpolation.cpp
+++ src/LagrangeInterpolation.cpp
@@ -11,24 +11,41 @@
  */
 
 /**
+ * Usage:
+ * - Create new Lagrange_5pnt oject using "Lagrange_5pnt l;"
+ * - Call the set parameters function using "l.set_parameters(double max_z, double setpoint_distance)"
+ * - When you want an updated Z-coordinate, call "get_z_value(double distance_to_setpoint)"
+ */
+
+/**
  * @brief Construct a new Lagrange_5pnt::Lagrange_5pnt object
- * 
  */
 Lagrange_5pnt::Lagrange_5pnt()
 {
-    
+        
 }
 
-void Lagrange_5pnt::set_lagrange_parameters(double max_z)
+/**
+ * @brief Sets the basic parameters and sets the control points/nodes
+ * 
+ * @param max_z [double] ==> The maximum height the leg z-path should reach
+ * @param setpoint_distance [double] ==> Distance between the start-point and end point in the XY plane
+ */
+void Lagrange_5pnt::set_lagrange_parameters(double max_z, double setpoint_distance)
 {
+    // A larger alpha value increases the initial steepness of the z-curve
+    // A larger beta increases the height of intermediate control point 
+
     double alpha = FUNCTION_SHAPE_ALPHA;
     double beta =  FUNCTION_SHAPE_BETA;
 
+    // Each z_pnt[] component is a z-coordinate that the path goes through
+    //  With corrresponding t_pnt[] components with dimmension [distance to endpoint(XY-plane)] or target_position(XY-plane) - current_position(XY-plane)
     z_pnt[0] = 0;
     z_pnt[2] = max_z; 
     double z_offset = beta * (z_pnt[2] - z_pnt[0]);
     
-    t_pnt[4] = 0; // *t_max?*
+    t_pnt[4] = setpoint_distance;
 
     t_pnt[0] = 0;
     t_pnt[2] = (t_pnt[0]+t_pnt[4]) / 2;
@@ -41,6 +58,7 @@ void Lagrange_5pnt::set_lagrange_parameters(double max_z)
     z_pnt[3] = z_pnt[1];
     z_pnt[4] = z_pnt[0];
 
+    // From desmos example, each param[] component is y_* / denominator
     param[0] = z_pnt[0] / (t_pnt[0]-t_pnt[1])*(t_pnt[0]-t_pnt[2])*(t_pnt[0]-t_pnt[3])*(t_pnt[0]-t_pnt[4]);
     param[1] = z_pnt[1] / (t_pnt[1]-t_pnt[0])*(t_pnt[1]-t_pnt[2])*(t_pnt[1]-t_pnt[3])*(t_pnt[1]-t_pnt[4]);    
     param[2] = z_pnt[2] / (t_pnt[2]-t_pnt[0])*(t_pnt[2]-t_pnt[1])*(t_pnt[2]-t_pnt[3])*(t_pnt[2]-t_pnt[4]);    
@@ -48,14 +66,20 @@ void Lagrange_5pnt::set_lagrange_parameters(double max_z)
     param[4] = z_pnt[4] / (t_pnt[4]-t_pnt[0])*(t_pnt[4]-t_pnt[1])*(t_pnt[4]-t_pnt[2])*(t_pnt[4]-t_pnt[3]);
 }
 
-double Lagrange_5pnt::get_z_value(double t)
+/**
+ * @brief Returns the z-value for the leg during its swing phase
+ * 
+ * @param setpoint_distance [double] ==> Distance to the setpoint (e.g. pass set_point-current_position)
+ * @return  [double] ==> Z-value for the leg during its swing phase
+ */
+double Lagrange_5pnt::get_z_value(double setpoint_distance)
 {
-    double return_value =
-    (t-t_pnt[1])*(t-t_pnt[2])*(t-t_pnt[3])*(t-t_pnt[4]) * param[0] +
-    (t-t_pnt[0])*(t-t_pnt[2])*(t-t_pnt[3])*(t-t_pnt[4]) * param[0] +
-    (t-t_pnt[0])*(t-t_pnt[1])*(t-t_pnt[3])*(t-t_pnt[4]) * param[0] +
-    (t-t_pnt[0])*(t-t_pnt[1])*(t-t_pnt[2])*(t-t_pnt[4]) * param[0] +
-    (t-t_pnt[0])*(t-t_pnt[1])*(t-t_pnt[2])*(t-t_pnt[3]) * param[0];
+    double z_value =
+    (setpoint_distance-t_pnt[1])*(setpoint_distance-t_pnt[2])*(setpoint_distance-t_pnt[3])*(setpoint_distance-t_pnt[4]) * param[0] +
+    (setpoint_distance-t_pnt[0])*(setpoint_distance-t_pnt[2])*(setpoint_distance-t_pnt[3])*(setpoint_distance-t_pnt[4]) * param[1] +
+    (setpoint_distance-t_pnt[0])*(setpoint_distance-t_pnt[1])*(setpoint_distance-t_pnt[3])*(setpoint_distance-t_pnt[4]) * param[2] +
+    (setpoint_distance-t_pnt[0])*(setpoint_distance-t_pnt[1])*(setpoint_distance-t_pnt[2])*(setpoint_distance-t_pnt[4]) * param[3] +
+    (setpoint_distance-t_pnt[0])*(setpoint_distance-t_pnt[1])*(setpoint_distance-t_pnt[2])*(setpoint_distance-t_pnt[3]) * param[4];
 
-    return return_value;
+    return z_value;
 }
